d85b128303e820f7a44c7e89a303026a9609c467
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2025-07-27 22:19:25 +0100

    chore: initial commit

---
+++ rspress.config.ts
@@ -0,0 +1,24 @@
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'rspress/config';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'My Site',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/web-infra-dev/rspress',
+      },
+    ],
+  },
+});
